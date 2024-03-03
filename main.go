771e8df6a2905ccd58ca9ce4d3037475dd46b372
romadsen <rolfrm@gmail.com>
2024-03-03 23:59:27 +0100

    nodes

--- main.go
+++ main.go
@@ -8,28 +8,38 @@ import (
 	vec4 "github.com/supersdf-go/engine/vec4"
 )
 
-type Entity struct {
-	Polygons []Polygon
-
-	Transform Mat4
+type Node struct {
+	polygon   *Polygon
+	subnodes  []Node
+	transform Mat4
 }
 
 type Game struct {
-	Entities []*Entity
+	Entities []*Node
 	time     float32
+}
+
+func (n *Node) Draw(screen Screen, tform Mat4) {
+	tform = tform.Multiply(n.transform)
+	if n.polygon != nil {
+		screen.Draw(*n.polygon, tform, n.polygon.Color)
+
+	}
+	if n.subnodes != nil {
+		for _, node := range n.subnodes {
+			node.Draw(screen, tform)
+		}
+	}
 }
 
 func (g *Game) Draw(screen Screen) {
 	proj := PerspectiveMatrix(1.2, 1.0, 0.01, 1000.0)
-	//proj := Mat4Identity()
 
 	screen.SetCamera(proj, vec3.New(0, 0, -5), vec3.New(0, 1, 0), vec3.New(float32(math.Cos(float64(g.time))), 0, float32(math.Sin(float64(g.time)))))
 
 	for _, e := range g.Entities {
-		for _, p := range e.Polygons {
-			screen.Draw(p, e.Transform, p.Color)
+		e.Draw(screen, Mat4Identity())
 
-		}
 	}
 }
 
@@ -51,7 +61,7 @@ func (g *Game) Update() {
 			vec3.New(-1, 1, 0),
 			vec3.New(1, 1, 0),
 		})
-		e1 := Entity{Polygons: []Polygon{p1}, Transform: Mat4Scale(0.5, 0.5, 0.5)}
+		e1 := Node{polygon: &p1, transform: Mat4Scale(0.5, 0.5, 0.5)}
 
 		p2 := Polygon{Color: vec4.New(1.0, 0.0, 0.0, 1.0)}
 		p2.Load3D([]vec3.Vec3{
@@ -59,9 +69,9 @@ func (g *Game) Update() {
 			vec3.New(0.0, 1, 0),
 			vec3.New(1, -1, 0),
 		})
-		e2 := Entity{Polygons: []Polygon{p2}, Transform: Mat4Scale(0.9, 0.9, 0.9)}
+		e2 := Node{polygon: &p2, transform: Mat4Scale(0.9, 0.9, 0.9)}
 
-		g.Entities = []*Entity{&e1, &e2}
+		g.Entities = []*Node{&e1, &e2}
 	}
 	//for _, e := range g.Entities {
 	//e.Transform = RotationMatrix(g.time, vec3.New(0, 0, 1))
